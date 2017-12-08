6705e58182faf2ed394cd5b3195d1e8e0e6771b0
Niyaz <Mail>
2017-12-08 13:42:35 +0300

    Добавьте файлы проекта.

---
+++ PharmacyAPI/Controllers/UserInfoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PharmacyAPI.Controllers
+{
+    
+    public class UserInfoController : ApiController
+    {
+        Pharmacy db = new Pharmacy();
+
+        [HttpGet]
+         public IQueryable Get(string login)
+        {
+            return db.UsersInfoes.Where(p => p.login == login);
+        }
+
+        
+    }
+}
